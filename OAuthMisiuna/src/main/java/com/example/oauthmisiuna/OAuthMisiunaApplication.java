cdf42d7e7f0e55fbdde24394d75670bf74cbcb37
Marcin Misiuna <72617994+marcmis979@users.noreply.github.com>
2023-05-24 09:21:19 +0200

    innitial commit

---
+++ OAuthMisiuna/src/main/java/com/example/oauthmisiuna/OAuthMisiunaApplication.java
@@ -0,0 +1,13 @@
+package com.example.oauthmisiuna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuthMisiunaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OAuthMisiunaApplication.class, args);
+    }
+
+}
---
+++ OAuthMisiuna/src/test/java/com/example/oauthmisiuna/OAuthMisiunaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oauthmisiuna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuthMisiunaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
